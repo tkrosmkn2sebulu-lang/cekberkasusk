ad425bc9fbf3477b0c4051ed07aa1bb3e7935ce7
tkrosmkn2sebulu-lang <tkrosmkn2sebulu@gmail.com>
2026-03-07 06:54:01 +0800

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-});
-
-self.addEventListener('fetch', (e) => {
-  // Biarkan kosong untuk sekadar memancing browser agar memunculkan tombol install
-});
